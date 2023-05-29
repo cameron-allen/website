3d225ec6d8ab327fb8ad44edbd1ba2f446eb4395
cameron-allen <cameronallen86@yahoo.com>
2023-05-28 20:41:44 -0700

    added social linked and better image

---
+++ src/NavBar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import "./index.css";
+
+import { IconContext } from 'react-icons';
+import { CiLinkedin, CiMail } from 'react-icons/ci';
+import { AiFillGithub } from 'react-icons/ai';
+
+function App() {
+
+  const handleCopy = async () => {
+    const email = "cameronallen86@gmail.com";
+
+    try {
+      await navigator.clipboard.writeText(email);
+      console.log('Email copied: ', email);
+    }catch (error) {
+      console.error('Copy failed:', error);
+    }
+  };
+
+  return (
+    <Navbar className="top-bar px-2" variant="dark">
+      <IconContext.Provider value={{ color: "white" }}>
+          <Nav>
+            <a href="https://www.linkedin.com/in/cameron-allen-54a02b207/"
+            target="_blank" rel="noopener noreferrer" style={{marginLeft: '.25rem'}}>
+              <CiLinkedin size={50} />
+            </a>
+            <a href="https://github.com/cameron-allen"
+            target="_blank" rel="noopener noreferrer" id="social">
+              <AiFillGithub size={50} />
+            </a>
+            <a href="#" id="social" onClick={handleCopy}>
+              <CiMail size={50} />
+            </a>
+          </Nav>
+        </IconContext.Provider>
+    </Navbar>
+  );
+}
+
+export default App;
