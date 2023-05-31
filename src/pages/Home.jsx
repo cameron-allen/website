624e9ced936c026a50e16ad6511c0fe29a826ceb
cameron-allen <cameronallen86@yahoo.com>
2023-05-30 18:51:06 -0700

    grammatical change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ export class Home extends React.Component {
                 src="https://drive.google.com/uc?export=view&id=1rBv5CNRnZR2MHsY8DDmCOuMWwyRUbdS8" alt="" />
                 <div className='col text-white responsive-text'>
                     <h1 style={{fontWeight: 'bold'}}>About Me</h1>
-                    <p className='bio'>My name is Cameron Allen; an SMU graduate who likes 
+                    <p className='bio'>My name is Cameron Allen—an SMU graduate who likes 
                     to code, draw and play the piano. I want to pursue a career in software development.
                     Check out my socials and GitHub to see what I'm up to.</p>
                 </div>
