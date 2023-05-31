f33b57c7c73175d1eb52ebe165025d737b498b42
cameron-allen <cameronallen86@yahoo.com>
2023-05-30 18:45:36 -0700

    updated 'about me'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,12 @@ export class Home extends React.Component {
             <div className="container mt-5 d-flex 
             justify-content-center">
                 <img className="col max-size-img" 
-                src="https://drive.google.com/uc?export=view&id=1CPimAyPh2O8XtY9iScvQosii5FKHlEBv" alt="" />
+                src="https://drive.google.com/uc?export=view&id=1rBv5CNRnZR2MHsY8DDmCOuMWwyRUbdS8" alt="" />
                 <div className='col text-white responsive-text'>
                     <h1 style={{fontWeight: 'bold'}}>About Me</h1>
-                    <p>I like cheese.</p>
+                    <p className='bio'>My name is Cameron Allen; an SMU graduate who likes 
+                    to code, draw and play the piano. I want to pursue a career in software development.
+                    Check out my socials and GitHub to see what I'm up to.</p>
                 </div>
             </div>
         </>;
