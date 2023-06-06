211a41df1eb39d665172a0a4e1609640bab85023
cameron-allen <cameronallen86@yahoo.com>
2023-06-05 20:42:32 -0700

    changed image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ export class Home extends React.Component {
             <div className="container mt-5 d-flex 
             justify-content-center">
                 <img className="col max-size-img" 
-                src="https://drive.google.com/uc?export=view&id=1rBv5CNRnZR2MHsY8DDmCOuMWwyRUbdS8" alt="" />
+                src="https://drive.google.com/uc?export=view&id=1M_ftqzXrwrFyVcCeww0ZdS9WaeO6EnVY" alt="" />
                 <div className='col text-white responsive-text'>
                     <h1 style={{fontWeight: 'bold'}}>About Me</h1>
                     <p className='bio'>My name is Cameron Allen—an SMU graduate who likes 
