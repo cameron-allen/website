3a7d5d4e5140968c9373c2557d97123e6eaffd2c
Cameron Allen <cameronallen86@yahoo.com>
2023-09-05 00:22:46 -0700

    changed bio

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,9 +14,7 @@ export class Home extends React.Component {
                 <Image className="col max-size-img" src={googleDriveURL} alt="SVG Image" />
                 <div className='col text-white responsive-text'>
                     <h1 style={{fontWeight: 'bold'}}>About Me</h1>
-                    <p className='bio'>My name is Cameron Allen—an SMU alumnus and current MS in Computer Science student
-                    at DigiPen Institute of Technology. I like to code, draw, write and play the piano. I want to pursue a career in video game or software development.
-                    Check out my socials and GitHub to see what I'm up to.</p>
+                    <p className='bio'>I'm Cameron Allen, an SMU alumnus pursuing an MS in Computer Science at DigiPen Institute of Technology. I'm passionate about coding, drawing, writing, and playing the piano. I want to pursue a career in video game or software development. Please check out my socials and GitHub to see what I'm up to.</p>
                 </div>
             </div>
         </>;
